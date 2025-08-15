046a5c04376ce36e9398b764a69c0ee2a5f5cb72
MP Data - Trading <mpiccato@me.com>
2025-08-15 07:48:40 -0300

    creando rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { LayoutMain } from './components/layouts/LayoutMain.tsx'
 import './index.css'
-import App from './App.tsx'
+import { Cart } from './pages/Cart/Cart.tsx'
+import { Home } from './pages/Home/Home.tsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LayoutMain/>,
+    children: [
+      {index:true, element: <Home/>},
+      {path: '/cart',element: <Cart/>}
+    ]
+  }
+]) // Assuming you will set up routes later
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
