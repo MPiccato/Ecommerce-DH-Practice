61e73301a3f398d2fde9f106c4e82bddeba640bf
MP Data - Trading <mpiccato@me.com>
2025-08-16 07:33:18 -0300

    llamada a la api de datos terminada

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,27 @@
 import { Hero } from "../../components/ui/Hero/Hero"
-
+import { useEffect, useState } from "react";
 export const Home = () => {
-  return (
+  
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products');
+      const data = await response.json()
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+ 
+  }, []);
+
+  console.log(products);
+  
+    return (
     <>
       <Hero />
     </>
