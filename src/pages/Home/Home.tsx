5597e5310fbf2e5c5c132082a7e5b8c24d9ef533
MP Data - Trading <mpiccato@me.com>
2025-08-17 06:58:56 -0300

    solo cambio el id

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,15 +14,6 @@ export const Home = () => {
   const [isLoading, setisLoading] = useState(true);
 
 
-  // const getProducts = async () => {
-  //   try {
-  //     const response = await fetch('http://localhost:3000/products');
-  //     const data = await response.json()
-  //     setProducts(data);
-  //   } catch (error) {
-  //     console.error("Error fetching products:", error);
-  //   }
-  // }
 
   useEffect(() => {
     getProducts().then((data) => {
@@ -44,7 +35,7 @@ export const Home = () => {
       <div className={styles.container}>
         {products.map((product)=> (
           <Card 
-            key={product.tail}
+            key={product.id}
             product={product}/>
         ))}
 
